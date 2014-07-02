4801a4da7fefdc89a3fd5ae088f89c0d944f3631
Subramanya Sastry <sss.lists@gmail.com>
2014-07-01 19:00:10 -0500

    Quick evening hack to generate visual diffs of different pages.
    * Made php and parsoid output more identical to eliminate false
      diffs.
    * Updated scripts to accept command line args.
    * More cleanup to be done, but starting to be more usable now.

--- gen.screenshots.js
+++ gen.screenshots.js
@@ -1,9 +1,95 @@
 var p = require('phantom'),
-    resemble = require('resemble').resemble;
+    resemble = require('resemble').resemble,
+	yargs = require('yargs');
 
 var phpDone = false;
 var parsoidDone = false;
 var browser;
+
+var wikiBaseUrl = {};
+
+var wikipedias = "en|de|fr|nl|it|pl|es|ru|ja|pt|zh|sv|vi|uk|ca|no|fi|cs|hu|ko|fa|id|tr|ro|ar|sk|eo|da|sr|lt|ms|eu|he|sl|bg|kk|vo|war|hr|hi|et|az|gl|simple|nn|la|th|el|new|roa-rup|oc|sh|ka|mk|tl|ht|pms|te|ta|be-x-old|ceb|br|be|lv|sq|jv|mg|cy|lb|mr|is|bs|yo|an|hy|fy|bpy|lmo|pnb|ml|sw|bn|io|af|gu|zh-yue|ne|nds|ku|ast|ur|scn|su|qu|diq|ba|tt|my|ga|cv|ia|nap|bat-smg|map-bms|wa|kn|als|am|bug|tg|gd|zh-min-nan|yi|vec|hif|sco|roa-tara|os|arz|nah|uz|sah|mn|sa|mzn|pam|hsb|mi|li|ky|si|co|gan|glk|ckb|bo|fo|bar|bcl|ilo|mrj|fiu-vro|nds-nl|tk|vls|se|gv|ps|rue|dv|nrm|pag|koi|pa|rm|km|kv|udm|csb|mhr|fur|mt|wuu|lij|ug|lad|pi|zea|sc|bh|zh-classical|nov|ksh|or|ang|kw|so|nv|xmf|stq|hak|ay|frp|frr|ext|szl|pcd|ie|gag|haw|xal|ln|rw|pdc|pfl|krc|crh|eml|ace|gn|to|ce|kl|arc|myv|dsb|vep|pap|bjn|as|tpi|lbe|wo|mdf|jbo|kab|av|sn|cbk-zam|ty|srn|kbd|lo|ab|lez|mwl|ltg|ig|na|kg|tet|za|kaa|nso|zu|rmy|cu|tn|chr|got|sm|bi|mo|bm|iu|chy|ik|pih|ss|sd|pnt|cdo|ee|ha|ti|bxr|om|ks|ts|ki|ve|sg|rn|dz|cr|lg|ak|tum|fj|st|tw|ch|ny|ff|xh|ng|ii|cho|mh|aa|kj|ho|mus|kr|hz|tyv|min";
+
+wikipedias.split('|').forEach(function(lang) {
+	// Wikipedia
+	var dbLangPrefix = lang.replace(/-/g, '_');
+	wikiBaseUrl[dbLangPrefix + 'wiki'] = 'http://' + lang +
+			'.wikipedia.org/wiki/';
+
+	// Wiktionary
+	wikiBaseUrl[dbLangPrefix + 'wiktionary'] = 'http://' + lang +
+			'.wiktionary.org/wiki/';
+
+	// Wikivoyage, Wikibooks, Wikisource, Wikinews, Wikiquote & Wikiversity
+	// all follow the same pattern
+	['voyage', 'books', 'source', 'news', 'quote', 'versity']
+		.forEach(function(suffix) {
+			wikiBaseUrl[dbLangPrefix + 'wiki' + suffix] = 'http://' +
+				lang + '.wiki' + suffix + '.org/wiki/';
+		});
+});
+
+var standardOpts = {
+	'help': {
+		description: 'Show this help message',
+		'boolean': true,
+		'default': false,
+		alias: 'h'
+	},
+	'wiki': {
+		description: 'Which wiki prefix to use; e.g. "enwiki" for English wikipedia, "eswiki" for Spanish, "mediawikiwiki" for mediawiki.org',
+		'boolean': false,
+		'default': 'enwiki'
+	},
+	'title': {
+		description: 'Which page title to use?',
+		'boolean': false,
+		'default': 'Main_Page'
+	},
+	'fileprefix': {
+		description: 'Prefix of files to output screenshots to? (default: <title>.php.png and <title>.parsoid.png)',
+		'boolean': false,
+		'default': null,
+	},
+	'outdir': {
+		description: 'Where to dump output?',
+		'boolean': false,
+		'default': '.'
+	},
+	'parsoidServer': {
+		description: 'What parsoid server to fetch Parsoid Html from?',
+		'boolean': false,
+		'default': 'http://localhost:8000/'
+	}
+};
+
+function checkUnknownArgs(standardOpts, argv, aliases) {
+	var knownArgs = Object.keys(aliases).reduce(function (prev, next) {
+		return prev.concat(aliases[next]);
+	}, ["_", "$0"].concat(Object.keys(standardOpts).map(function(arg) {
+		return arg.split(" ")[0];
+	})));
+
+	Object.keys(argv).forEach(function (arg) {
+		if ( knownArgs.indexOf(arg) < 0 ) {
+			throw "Unknown argument: " + arg;
+		}
+	});
+}
+
+var opts   = yargs.usage('Usage: node gen.screenshots.js [options]', standardOpts).check(checkUnknownArgs.bind(null, standardOpts));
+var argv   = opts.argv;
+var title  = argv.title;
+var outdir = (argv.outdir || "./").replace(/\/$/, '') + "/";
+var prefix = argv.fileprefix || title;
+var wiki   = argv.wiki;
+var phpServer = wikiBaseUrl[wiki];
+var psdServer = argv.parsoidServer;
+
+if (argv.help) {
+	opts.showHelp();
+	return;
+}
 
 p.create(function (ph) {
   browser = ph;
@@ -13,14 +99,18 @@ p.create(function (ph) {
 	page.set('viewportSize', { width: 1920, height: 1080 }, function (result) {
 	  console.log("Viewport set to: " + result.width + "x" + result.height);
 	});
-    page.open("https://en.wikipedia.org/wiki/Medha_Patkar", function (status) {
+    page.open(phpServer + title, function (status) {
 	  page.includeJs("http://ajax.googleapis.com/ajax/libs/jquery/1.6.1/jquery.min.js", function() {
 	    page.evaluate(function() {
+			// Open navboxes
+			$("table.collapsible tr").show();
+
 			// Hide navigation
 			$("div#mw-page-base").hide();
 			$("div#mw-head-base").hide();
 			$("div#mw-navigation").hide();
 			$("div#footer").hide();
+			$("div.suggestions").hide();
 
 			// Hide site notice and other anchors
 			$("a#top").hide();
@@ -38,13 +128,15 @@ p.create(function (ph) {
 			// Hide edit links
 			$("span.mw-editsection").hide();
 
-			// Hide catlinks
+			// Hide catlinks + footer
+			$("div.printFooter").hide();
 			$("div#catlinks").hide();
+			$("div#catlinks+div.visualClear").hide();
 
-			// Open navboxes
-			$("table.collapsible tr").show();
+			// Hide show/hide buttons
+			$("span.collapseButton").hide();
 		});
-	    page.render("mp.php.png", function() {
+	    page.render(outdir + prefix + ".php.png", function() {
 			console.warn("php done!");
 			phpDone = true;
 			if (parsoidDone) {
@@ -59,7 +151,7 @@ p.create(function (ph) {
 	page.set('viewportSize', { width: 1920, height: 1080 }, function (result) {
 	  console.log("Viewport set to: " + result.width + "x" + result.height);
 	});
-    page.open("http://parsoid.wmflabs.org/enwiki/Medha_Patkar", function (status) {
+    page.open(psdServer + wiki + "/" + title, function (status) {
 	  page.includeJs("http://ajax.googleapis.com/ajax/libs/jquery/1.6.1/jquery.min.js", function() {
 	    page.evaluate(function() {
 		  var body = document.body;
@@ -100,7 +192,7 @@ p.create(function (ph) {
 		}
 		);
 
-	    page.render("mp.parsoid.png", function() {
+	    page.render(outdir + prefix + ".parsoid.png", function() {
 			console.warn("parsoid done!");
 			parsoidDone = true;
 			if (phpDone) {
--- gen.visual_diff.js
+++ gen.visual_diff.js
@@ -1,9 +1,23 @@
 var resemble = require('resemble').resemble,
-	c = require('./node_modules/resemble/node_modules/canvas/lib/canvas.js');
+	c = require('./node_modules/resemble/node_modules/canvas/lib/canvas.js'),
+	sys = require('sys'),
+	exec = require('child_process').exec,
+	fs = require('fs');
 
-resemble("mp.php.png").compareTo("mp.parsoid.png").
+var argv   = process.argv;
+var prefix = argv.length > 2 ? argv[2] : "Medha_Patkar";
+
+resemble(prefix + ".php.png").compareTo(prefix + ".parsoid.png").
 	ignoreAntialiasing(). // <-- muy importante
 	onComplete(function(data){
+	  // analysis stats
 	  console.error("STATS: " + JSON.stringify(data));
-	  console.log(data.getImageDataUrl("").replace(/^data:image\/png;base64,/, ''));
+
+	  // convert png data to a jpeg
+	  var png_data = data.getImageDataUrl("").replace(/^data:image\/png;base64,/, '');
+	  var png_file = "/tmp/" + prefix + ".diff.png.data";
+	  var jpg_file = prefix + ".diff.jpg";
+	  fs.writeFileSync(png_file, png_data, "utf8");
+	  function puts(error, stdout, stderr) { sys.puts(stdout); }
+	  exec("/usr/bin/base64 -d < " + png_file + " > " + jpg_file, puts);
 	});
