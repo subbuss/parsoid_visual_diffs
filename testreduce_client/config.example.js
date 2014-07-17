1c0319eb24420861d88af5ec374f21b9a8e9e225
Subramanya Sastry <sss.lists@gmail.com>
2014-07-17 17:46:34 -0500

    Added visual diffing testreduce client.
    * Initial tests good.
    * More thorough testing required.

---
+++ testreduce_client/config.example.js
@@ -0,0 +1,28 @@
+/**
+ * Example configuration for the visual diffing client.
+ *
+ * Copy this file to config.js and change the values as needed.
+ */
+"use strict";
+
+if ( typeof module === 'object' ) {
+	module.exports = {
+		server: {
+			// The address of the master HTTP server (for getting titles and posting results) (no protocol)
+			host: 'localhost',
+
+			// The port where the server is running
+			port: 8002
+		},
+		opts: {
+			parsoidServer: "http://localhost:8000",
+			viewportWidth: 1920,
+			viewportHeight: 1080,
+			dumpParsoidHTML: false,
+			wiki: "enwiki",
+			title: "Main_Page",
+			filePrefix: null,
+			outdir: null
+		}
+	};
+}
